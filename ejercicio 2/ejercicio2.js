8d9ae3625e468d2371611e0e765a1298c770ab2b
“Lucas <lukestraik10@gmail.com>
2023-10-26 01:45:46 +0200

    Retoques varios, empieza a funcionar

--- ejercicio 2/ejercicio2.js
+++ ejercicio 2/ejercicio2.js
@@ -1,33 +1,36 @@
 //import {Grupo} from "./grupo.js";
 
-
-
 class Grupo{
+    codigoGrupo;
+    numero;
     constructor(codigoGrupo, numero){
         this._codigoGrupo=codigoGrupo;
         this._numero=numero;
     }
 
-    set codigoGrupo(Grupo){
+    set CodigoGrupo(Grupo){
         this._codigoGrupo=Grupo;
     }
-    get codigoGrupo(){
+    get CodigoGrupo(){
         return this._codigoGrupo;
     }
-    set numero(num){
+    set Numero(num){
         this._numero=num;
     }
-    get numero(){
+    get Numero(){
         return this._numero;
     }
 
 }
 
-class Instituto extends Grupo{
-    constructor(codigoInstituto, nombre, Grupo){
+class Instituto{
+    codigoInstituto;
+    nombre;
+    grupos;
+    constructor(codigoInstituto, nombre, grupos){
         this._codigoInstituto=codigoInstituto;
         this._nombre=nombre;
-        this._grupos=new Grupo();
+        this._grupos=grupos;
     }
 
     set CodigoInstituto(codigo){
@@ -48,22 +51,21 @@ class Instituto extends Grupo{
     get Grupos(){
         return this._grupos;
     }
-    
-}
 
-
-function codigoInsti(){
-    console.log("El código del instituto es:" ,Instituto.CodigoInstituto());
-}
-
-
-function numeroAlumnos(){
-    console.log("El código del instituto es:" ,Grupo.numeroAlumnos());
-}
-
-function addGrupos(){
+    codigoInsti(){
+        console.log('El código del instituto es: '+ this.CodigoInstituto);
+    }
+    numeroAlumnos(){
+        /*for(let i of this.Grupos){
+            SUMATORIO DE TODOS, FALTA  QUE SE SUMEN Y AGREGARLOS
+        }*/
+        console.log("El numero de alumnos es: "+this.Grupos.Numero);
+    }
+    addGrupos(){
    
-}
+    }
+    
+} 
 
 
 var grupo1=new Grupo(1,20);
@@ -74,4 +76,6 @@ var insti1=new Instituto(1,"Fleming",grupo1);
 var insti2=new Instituto(1,"Fleming",grupo2);
 var insti1=new Instituto(1,"Fleming",grupo3);
 
-console.log(insti1.codigoInstituto);
+console.log(insti1.CodigoInstituto);
+insti1.codigoInsti();
+insti1.numeroAlumnos();
